2e4239ef912c2257afb37c8beee85c90d0ef5543
HagaSpa <justicesparrow13@gmail.com>
2020-08-08 11:57:41 +0900

    Add test case for codewriter writeBinaryOperator()

--- vmt/codewriter/codewriter_test.go
+++ vmt/codewriter/codewriter_test.go
@@ -15,7 +15,6 @@ func TestCodeWriter_writeBinaryOperator(t *testing.T) {
 		args    string
 		want    string
 	}{
-		// TODO: Add test cases.
 		{
 			"add",
 			"add",
@@ -34,6 +33,60 @@ M=D+M
 M=M+1
 `,
 		},
+		{
+			"sub",
+			"sub",
+			"M=M-D",
+			`
+// Binary Operator M=M-D
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+M=M-D
+@SP
+M=M+1
+`,
+		},
+		{
+			"and",
+			"and",
+			"M=D&M",
+			`
+// Binary Operator M=D&M
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+M=D&M
+@SP
+M=M+1
+`,
+		},
+		{
+			"or",
+			"or",
+			"M=D|M",
+			`
+// Binary Operator M=D|M
+@SP
+M=M-1
+A=M
+D=M
+@SP
+M=M-1
+A=M
+M=D|M
+@SP
+M=M+1
+`,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
@@ -46,7 +99,7 @@ M=M+1
 			cw.writeBinaryOperator(tt.args)
 
 			if string(b.Bytes()) != tt.want {
-				t.Errorf("riteBinaryOperator() = %s, want %v", b, tt.want)
+				t.Errorf("writeBinaryOperator() = %s, want %v", b, tt.want)
 			}
 		})
 	}
