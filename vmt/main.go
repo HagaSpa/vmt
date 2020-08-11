88fa44babbe51c51d786222209be1aafc575a2a5
HagaSpa <justicesparrow13@gmail.com>
2020-08-11 18:05:59 +0900

    Update main.go

--- vmt/main.go
+++ vmt/main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	"vmt/parser"
+)
 
 func main() {
-	fmt.Println("hello world")
+	// parse args
+	flag.Parse()
+	flags := flag.Args()
+	if flags == nil {
+		os.Exit(1)
+	}
+
+	// open vm
+	f, err := os.Open(flags[0])
+	if err != nil {
+		os.Exit(1)
+	}
+	defer f.Close()
+
+	// generate parser
+	p := parser.New(f)
+	for p.HasMoreCommands() {
+		p.Advance()
+		fmt.Println(p)
+		// TODO: switch Type
+	}
+
 }
